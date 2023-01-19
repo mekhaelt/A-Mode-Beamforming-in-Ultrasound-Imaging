e692b3a25443e95574ae10d400ac3fd6b7322886
mekhaelt <106632379+mekhaelt@users.noreply.github.com>
2023-01-19 14:25:54 -0500

    Updated comments to be more comprehensive

--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 int main () {
 
-    //This section initializes the constant values you will need
+    //Initializes the constant values
     const int N = 128; //Number of channels 
     const int K = 3338; //Number of Samples
-    const float PITCH = 3.048e-4; //This is the transducer pitch
+    const float PITCH = 3.048e-4; //The transducer pitch
     const float SOS = 1540; //Speed of sound variable
     const float FS = 40e6; // Sampling frequency variable
     const float TS = 1.0f/FS; //Sampling time variable
 
-    //Use this section to create your array of pointers, and the arrays the pointers point to.
+    //Create the array of pointers, and the arrays the pointers point to.
     float **realRFData = createDataMatrix(N, K);
     float **imagRFData = createDataMatrix(N, K);
 
     int result = 0;
 
-    //Use this section to input the data from the txt files. 
+    //Inputs the data from the txt files. 
     result = loadRFData(realRFData, "RealRFData_mystery.txt", N, K);
     if (result == -1)
     {
@@ -34,21 +34,20 @@ int main () {
         return -1;
     }
 
-    //Create your imaging location array here
+    //Creates imaging location array here
     int numPixel = 256;
     float *scanlinePosition = genScanlineLocation(numPixel);
 
-    //Create you element locations here
+    //Creates element locations here
     float* elementPosition; 
     elementPosition = genElementLocation(N,PITCH);
 
-    //Perform your A-mode beamforming here
+    //Performs A-mode beamforming here
     float *scanline;
     scanline = createScanline(numPixel);
     beamform(scanline, realRFData, imagRFData, scanlinePosition, elementPosition, N, K, numPixel, FS, SOS);
 
-    //Use this section to output your data into a .csv file. Make sure to seperate columns with commas and
-    //use new lines to separate the rows.
+    //Outputs data into a .csv file
     result = outputScanline("output.csv", scanlinePosition, scanline, numPixel);
     if (result == -1)
     {
